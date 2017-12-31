97347d800e4c74ce073511202e7e269cd10e8881
ejaycoleman <ejaycoleman@googlemail.com>
2017-12-31 12:47:57 +0000

    main methods implemented

--- FinchEclipse/src/navigate/Main.java
+++ FinchEclipse/src/navigate/Main.java
@@ -4,13 +4,37 @@ import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 
 public class Main {
 	public static void main(final String[] args) {
-		Finch myFinch = new Finch();
+		Finch finchInstance = new Finch();
 
-		myFinch.setLED(255, 255, 255);
-		myFinch.sleep(8000);
+		finchInstance.setLED(255, 255, 255);
+		finchInstance.sleep(8000);
 		
 		// Always end your program with finch.quit()
-		myFinch.quit();
+		quit(finchInstance);
+	}
+	
+	private static void forward(Finch finchInstance) {
+		
+	}
+	
+	private static void backward(Finch finchInstance) {
+		
+	}
+	
+	private static void rightTurn(Finch finchInstance) {
+		
+	}
+	
+	private static void leftTurn(Finch finchInstance) {
+		
+	}
+	
+	private static void reTraceMovements(Finch finchInstance) {
+		
+	}
+	
+	private static void quit(Finch finchInstance) {
+		finchInstance.quit();
 		System.exit(0);
 	}
 }
