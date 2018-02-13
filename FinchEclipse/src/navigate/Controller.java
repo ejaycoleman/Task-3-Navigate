ab5bc48d87adab10f0d272ad847aec9f5e8c2b30
ejaycoleman <ejaycoleman@googlemail.com>
2018-02-13 00:43:23 +0000

    time now in seconds. Unexpected inputs accounted for

--- FinchEclipse/src/navigate/Controller.java
+++ FinchEclipse/src/navigate/Controller.java
@@ -8,7 +8,6 @@ import edu.cmu.ri.createlab.terk.robot.finch.Finch;
 
 public class Controller {
 	public Controller() {
-		System.out.println("yes");
 		this.setUpFinch();
 		this.finchInstance.setLED(0, 255, 0);
 	}
@@ -25,9 +24,6 @@ public class Controller {
 		String inputString;
 		inputString = "";
 		
-		
-		
-		
 		int speed;
 		int time;
 		int quantity;
@@ -37,9 +33,14 @@ public class Controller {
 		while(inputString != "Q") {
 			inputString = getUserInput();
 			
+			// TODO check input gonna be valid
+			
+			// TODO multiply time input by 1000 so its in milliseconds 
+			
 			
 			if (inputString == null) {
-				this.quit();
+				//this.quit();
+				break;
 			}
 			
 			
@@ -52,67 +53,134 @@ public class Controller {
 			System.out.println("the stack: "+stack);
 			
 			String instructionOption = inputArray[0];
+			int[] testedParameters;
 			
-			
-			
+//			if (instructionOption == "F") {
+//				
+//			}
 			
 
 	        switch (instructionOption) {
 	            case "F":  
-	            		speed = Integer.parseInt(inputArray[1]);
-	            		time = Integer.parseInt(inputArray[2]);
+	            	
+	            		
+	            	
+//	            		speed = Integer.parseInt(inputArray[1]);
+//	            		time = Integer.parseInt(inputArray[2]);
+	            	
+	            		testedParameters = testParameters(inputArray, 3);
 
+	            		if (testedParameters != null) {
+	            			if (checkInRange(testedParameters[0], testedParameters[1])) {
+			        			stack.add(inputString);
+			        			forward(testedParameters[0], testedParameters[1]);
+			            		break;
+			        		} else {
+			        			break;
+			        		}
+	            		} else {
+	            			infoBox("The forward command requires two parameters only", "Unexpected Input");
+	            			break;
+	            		}
 	                
-	            		if (checkInRange(speed, time)) {
-		        			stack.add(inputString);
-		        			forward(speed, time);
-		            		break;
-		        		} else {
-		        			break;
-		        		}
+	            		
 	            		
 	            case "B":
-		            	speed = Integer.parseInt(inputArray[1]);
-		        		time = Integer.parseInt(inputArray[2]);
-	            		if (checkInRange(speed, time)) {
-		        			stack.add(inputString);
-		        			backward(speed, time);
-		            		break;
-		        		} else {
-		        			break;
-		        		}
+//		            	speed = Integer.parseInt(inputArray[1]);
+//		        		time = Integer.parseInt(inputArray[2]);
+//	            		if (checkInRange(speed, time)) {
+//		        			stack.add(inputString);
+//		        			backward(speed, time);
+//		            		break;
+//		        		} else {
+//		        			break;
+//		        		}
+		            	testedParameters = testParameters(inputArray, 3);
+	
+	            		if (testedParameters != null) {
+	            			if (checkInRange(testedParameters[0], testedParameters[1])) {
+			        			stack.add(inputString);
+			        			backward(testedParameters[0], testedParameters[1]);
+			            		break;
+			        		} else {
+			        			break;
+			        		}
+	            		} else {
+	            			infoBox("The back command requires two parameters only", "Unexpected Input");
+	            			break;
+	            		}
 	            case "R":
-		            	speed = Integer.parseInt(inputArray[1]);
-		        		time = Integer.parseInt(inputArray[2]);
-	            		
-	            		if (checkInRange(speed, time)) {
-		        			stack.add(inputString);
-		            		rightTurn(speed, time);
-		            		break;
-		        		} else {
-		        			break;
-		        		}
+//		            	speed = Integer.parseInt(inputArray[1]);
+//		        		time = Integer.parseInt(inputArray[2]);
+//	            		
+//	            		if (checkInRange(speed, time)) {
+//		        			stack.add(inputString);
+//		            		rightTurn(speed, time);
+//		            		break;
+//		        		} else {
+//		        			break;
+//		        		}
+	            	
+		            	testedParameters = testParameters(inputArray, 3);
+		            	
+	            		if (testedParameters != null) {
+	            			if (checkInRange(testedParameters[0], testedParameters[1])) {
+			        			stack.add(inputString);
+			        			rightTurn(testedParameters[0], testedParameters[1]);
+			            		break;
+			        		} else {
+			        			break;
+			        		}
+	            		} else {
+	            			infoBox("The right command requires two parameters only", "Unexpected Input");
+	            			break;
+	            		}
 	            		
 	            case "L":
 	            		
-		            	speed = Integer.parseInt(inputArray[1]);
-		        		time = Integer.parseInt(inputArray[2]);
-		        		if (checkInRange(speed, time)) {
-		        			stack.add(inputString);
-		            		leftTurn(speed, time);
-		            		break;
-		        		} else {
-		        			break;
-		        		}
-	            case "T":
-	            		quantity = Integer.parseInt(inputArray[1]);
-	            		if (quantity > stack.size()) {
-	            			infoBox("Stack cannot exceed: "+ stack.size(), "Stack Overflow!");
-	            			break;
+//		            	speed = Integer.parseInt(inputArray[1]);
+//		        		time = Integer.parseInt(inputArray[2]);
+//		        		if (checkInRange(speed, time)) {
+//		        			stack.add(inputString);
+//		            		leftTurn(speed, time);
+//		            		break;
+//		        		} else {
+//		        			break;
+//		        		}
+	            	
+		            	testedParameters = testParameters(inputArray, 3);
+		            	
+	            		if (testedParameters != null) {
+	            			if (checkInRange(testedParameters[0], testedParameters[1])) {
+			        			stack.add(inputString);
+			        			leftTurn(testedParameters[0], testedParameters[1]);
+			            		break;
+			        		} else {
+			        			break;
+			        		}
 	            		} else {
-			            	reTraceMovements(quantity, stack);
-			            	break;
+	            			infoBox("The left command requires two parameters only", "Unexpected Input");
+	            			break;
 	            		}
+	            		
+	            case "T":
+	            	
+		            	testedParameters = testParameters(inputArray, 2);
+		            	
+	            		if (testedParameters != null) {
+
+		            		if (testedParameters[1] > stack.size()) {
+		            			infoBox("Stack cannot exceed: "+ stack.size(), "Stack Overflow!");
+		            			break;
+		            		} else {
+				            	reTraceMovements(testedParameters[1], stack);
+				            	break;
+		            		}
+	            		} else {
+	            			infoBox("The trace command requires one parameter only", "Unexpected Input");
+	            			break;
+	            		}
+	            		
 	            case "Q":
 	            		quit();
 	            		break;
@@ -127,12 +195,31 @@ public class Controller {
 		quit();
 	}
 	
+	private int[] testParameters(String[] inputArray, int expected) {
+		int speed;
+		int time;
+		if (inputArray.length == expected) {
+			
+			try {
+				speed = Integer.parseInt(inputArray[1]);
+        			time = Integer.parseInt(inputArray[2]);
+			} catch (NumberFormatException e) {
+				return null;
+			}
+			int[] returnArray = {speed, time};
+			return returnArray;
+		} else {
+			return null;
+		}
+	}
+	
 	private boolean checkInRange(int time, int speed) {
-		if (speed <= 200 || speed < 0) {
+		if (speed > 200 || speed < 0) {
 			infoBox("Speed must be from 0 to 200", "Out Of Range");
+			System.out.println(speed);
 			return false;
-		} else if (time <= 200 || time < 0) {
-			infoBox("Time must be from 0 to 200", "Out Of Range");
+		} else if (time > 6 || time < 0) {
+			infoBox("Time must be from 0 to 6 seconds", "Out Of Range");
 			return false;
 		} else {
 			return true;
@@ -140,21 +227,21 @@ public class Controller {
 	}
 	
 	private void forward(int time, int speed) {
-		finchInstance.setWheelVelocities(speed, speed, time);
+		finchInstance.setWheelVelocities(speed, speed, time*1000);
 	}
 	
 	private void backward(int time, int speed) {
-		finchInstance.setWheelVelocities(-speed, -speed, time);
+		finchInstance.setWheelVelocities(-speed, -speed, time*1000);
 	}
 	
 	private void rightTurn(int time, int speed) {
-		finchInstance.setWheelVelocities(255, -255, 500);
-		finchInstance.setWheelVelocities(speed, speed, time);
+		finchInstance.setWheelVelocities(150, 0, 1000);
+		finchInstance.setWheelVelocities(speed, speed, time*1000);
 	}
 	
 	private void leftTurn(int time, int speed) {
-		finchInstance.setWheelVelocities(-255, 255, 500);
-		finchInstance.setWheelVelocities(speed, speed, time);
+		finchInstance.setWheelVelocities(0, 150, 1000);
+		finchInstance.setWheelVelocities(speed, speed, time*1000);
 	}
 	
 	private void reTraceMovements(int quantity, ArrayList<String> stack) {
